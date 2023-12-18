eb65a6e8e8b871298821d3600a384af86d40c61b
tyronmaster <degonski.art@gmail.com>
2023-12-18 13:17:15 +0100

    Auto-generated commit [ci skip]

---
+++ assets/images/index.ts
@@ -0,0 +1,12 @@
+export const SIGNS ={
+  '101-gefahr': '../images/101-gefahr.png',
+  '123-bauarbeiten': '../images/123-bauarbeiten.png',
+  '250-sperrung': '../images/250-sperrung.png',
+  '262-2-max3p5t': '../images/262-2-max3p5t.png',
+  '314-50-parkplatz-pkw': '../images/314-50-parkplatz-pkw.png',
+  '314-50-parkplatz-pkw-lkw': '../images/314-50-parkplatz-pkw-lkw.png',
+  '448-anschlussstelle-gesperrt': '../images/448-anschlussstelle-gesperrt.png',
+  'charging-plug-strong': '../images/charging-plug-strong.png',
+  'warnkegel': '../images/warnkegel.png',
+}
+
